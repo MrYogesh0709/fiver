c873dcc0cb6b7cd819650a49f1d2065be4519dc9
Mr.Yogesh <yogeshvanzara98@gmail.com>
2023-03-08 14:48:09 +0530

    changs

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -32,7 +32,7 @@ const login = async (req, res, next) => {
       .cookie("accessToken", token, {
         httpOnly: true,
         secure: true,
-        domain: "https://fiver-front.netlify.app",
+        domain: "fiver-front.netlify.app",
         sameSite: "none",
         path: "/",
       })
