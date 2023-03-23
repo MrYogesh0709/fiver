c52a4d54b8b5acc81e4618d1266fdc048728454a
Mr.Yogesh <yogeshvanzara98@gmail.com>
2023-03-23 19:09:03 +0530

    change

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -33,7 +33,7 @@ const login = async (req, res, next) => {
         httpOnly: true,
         sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
         secure: process.env.NODE_ENV === "Development" ? false : true,
-        domain: ".netlify.app",
+        // domain: ".netlify.app",
       })
       .status(200)
       .json({ info });
